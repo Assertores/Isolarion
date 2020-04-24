afbafcae7f7fb3727243d37905804421492cee61
Andreas Edmeier <assertores@web.de>
2020-04-24 22:32:56 +0200

    [PRO] added model random scriptable object

---
+++ Isolarion/Assets/Code/Tool/RandModelAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+	[System.Serializable]
+	public struct RanTexElement {
+		public Texture _albedo;
+		public Texture _hight;
+	}
+
+	[CreateAssetMenu(fileName = "RanTexAsset", menuName = "Game/RandModel")]
+	public class RandModelAsset : ScriptableObject {
+		public Mesh[] _meshs;
+		public RanTexElement[] _ranTexs;
+	}
+}
