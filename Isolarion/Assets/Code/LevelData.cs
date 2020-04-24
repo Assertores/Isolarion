c9a90168490c6502e57a11e766c9ec40a407f656
Andreas Edmeier <assertores@web.de>
2020-04-24 21:43:18 +0200

    [PRO] LevelData Scripatable Object

---
+++ Isolarion/Assets/Code/LevelData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iso {
+	[CreateAssetMenu(fileName = "Level", menuName = "Game/Level")]
+	public class LevelData : ScriptableObject {
+		public GameObject[] Shapes;
+		public Vector2[] Pins;
+	}
+}
