58ccfb6c7d20da73c41cddfeff4fb793cf296823
Andreas Edmeier <assertores@web.de>
2020-04-24 22:32:28 +0200

    [PRO] InputSystem

---
+++ Isolarion/Assets/Code/InputSystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AsserTOOLres;
+
+namespace Iso {
+	public class InputSystem : Singleton<InputSystem> {
+
+		[SerializeField] LayerMask shapeMask;
+		[SerializeField] LayerMask curserMask;
+
+		GameObject currentObject;
+		Vector3 offset;
+
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+			if(Input.GetMouseButtonDown(0)) {
+				offset = GetShape(out currentObject);
+			} else if(Input.GetMouseButtonUp(0)) {
+				currentObject = null;
+			}
+
+			if(currentObject) {
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
+				out RaycastHit hit,
+				1000.0f,
+				curserMask)) {
+
+					currentObject.transform.position = hit.point + offset;
+				}
+			}
+		}
+
+		Vector3 GetShape(out GameObject target) {
+			target = null;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
+				out RaycastHit hit,
+				1000.0f,
+				shapeMask)) {
+
+				// TODO: evel stuff dont yous root
+				target = hit.collider.transform.root.gameObject;
+
+				Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
+				out hit,
+				1000.0f,
+				curserMask);
+				return target.transform.position - hit.point;
+			}
+			return Vector3.zero;
+		}
+	}
+}
