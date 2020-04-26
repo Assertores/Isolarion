3cdc32a0e9da7849b257cc03af768f76ca29cc8e
Andreas Edmeier <assertores@web.de>
2020-04-26 11:33:03 +0200

    [PRO] added Skip capability

---
+++ Isolarion/Assets/Code/GUI/GUISkip.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iso {
+	public class GUISkip : MonoBehaviour {
+		public void Execute() {
+			if(LevelHandler.s_instance.NextLevel()) {
+				return;
+			}
+
+			Debug.Log("finished Last Level.");
+		}
+	}
+}
